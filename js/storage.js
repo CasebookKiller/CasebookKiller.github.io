669c8c1e41f9bc213a25302c6b36589d58761880
gymprogressive <114830159+gymprogressive@users.noreply.github.com>
2024-09-05 09:28:51 +0300

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -1,3 +1,9 @@
+window.addEventListener('message', function(msg) {
+  alert(msg.data); //Сообщение отправленно в storage.html
+  // читать здесь
+  // https://ru.stackoverflow.com/questions/1275278/%D0%9A%D0%B0%D0%BA-%D0%BE%D1%82%D0%BF%D1%80%D0%B0%D0%B2%D0%B8%D1%82%D1%8C-%D1%81%D0%BE%D0%BE%D0%B1%D1%89%D0%B5%D0%BD%D0%B8%D0%B5-%D0%B2-iframe-%D0%B8-%D0%BE%D0%B1%D1%80%D0%B0%D1%82%D0%BD%D0%BE
+});
+
 // Проверка IndexedDB
 window.indexedDB =
   window.indexedDB ||
