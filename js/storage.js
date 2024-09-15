b275095c3de91c59974430b106902ece7541c150
gymprogressive <114830159+gymprogressive@users.noreply.github.com>
2024-09-15 12:32:01 +0300

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -71,9 +71,15 @@ window.addEventListener('message', function(msg) {
         info = JSON.parse(info);
       } catch (err) {
       }
-      console.log('typeof: ',typeof(info));
-      console.log('%cсообщение поступило в storage.html: %o','color:#FB74BD;', info); //Сообщение отправленно в storage.html
-      // читать здесь
+
+      //Сообщение отправленно в storage.html
+      if (typeof(info) === 'object') {
+        console.log('%cв storage.html поступил объект: %o','color:#FB74BD;', info);
+      } else {
+        console.log('%cв storage.html поступило сообщение: ','color:#FB74BD;', info);
+      }
+      
+            // читать здесь
       // https://ru.stackoverflow.com/questions/1275278/%D0%9A%D0%B0%D0%BA-%D0%BE%D1%82%D0%BF%D1%80%D0%B0%D0%B2%D0%B8%D1%82%D1%8C-%D1%81%D0%BE%D0%BE%D0%B1%D1%89%D0%B5%D0%BD%D0%B8%D0%B5-%D0%B2-iframe-%D0%B8-%D0%BE%D0%B1%D1%80%D0%B0%D1%82%D0%BD%D0%BE
     }
     
