c79ed8dcb0af7a574b8c8cc7eed9f5fad66a38ee
gymprogressive <114830159+gymprogressive@users.noreply.github.com>
2024-09-15 10:46:30 +0300

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -55,12 +55,18 @@ if (!window.indexedDB) {
  */
 window.addEventListener('message', function(msg) {
   if (msg.origin === 'https://kad.arbitr.ru') {
-    let obj = JSON.parse(msg.data);
+    let obj = undefined;
     
+    try {
+      obj = JSON.parse(msg.data);
+    } catch (err) {
+      obj = msg.data;
+    }
+
     if (obj.key === 'storage') {
-      console.log('msg:',msg);
-      console.log('obj:',obj);
-      console.log('%cсообщение поступило в storage.html: %o','color:#FFA812;', msg.data); //Сообщение отправленно в storage.html
+      //console.log('msg:',msg);
+      //console.log('obj:',obj);
+      console.log('%cсообщение поступило в storage.html: %o','color:#FFA812;', obj.info); //Сообщение отправленно в storage.html
       // читать здесь
       // https://ru.stackoverflow.com/questions/1275278/%D0%9A%D0%B0%D0%BA-%D0%BE%D1%82%D0%BF%D1%80%D0%B0%D0%B2%D0%B8%D1%82%D1%8C-%D1%81%D0%BE%D0%BE%D0%B1%D1%89%D0%B5%D0%BD%D0%B8%D0%B5-%D0%B2-iframe-%D0%B8-%D0%BE%D0%B1%D1%80%D0%B0%D1%82%D0%BD%D0%BE
     }
