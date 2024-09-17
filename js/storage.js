8274cfdbe46ce06231bfb5177684af08e152ff89
gymprogressive <114830159+gymprogressive@users.noreply.github.com>
2024-09-17 09:48:03 +0300

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -70,7 +70,7 @@ window.addEventListener('message', function(msg) {
       let dates = obj.dates !== undefined ? obj.dates : '';
       try {
         info = JSON.parse(info);
-        info.Id = (new Date()).getMilliseconds();
+        info.Id = (new Date()).getTime();
       } catch (err) {
       }
 
